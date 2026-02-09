29e6b138294b0ae1da39da08e4ed55b6aa0ab40b
mcgirtc7353-cmyk <mcgirtc7353@student.faytechcc.edu>
2026-02-09 10:59:23 -0500

    Add files via upload

---
+++ P1LAB1.py
@@ -0,0 +1,12 @@
+# Cedric McGirt
+# 9/5/2024
+# PlLAB1.py 
+# This program ask the user for there first and last name 
+
+
+# Ask the user for their first and last name
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Display the full name
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110!.")
