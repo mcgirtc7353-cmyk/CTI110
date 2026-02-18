6dec275f616f0edb636a89b2d80c948328f3e0e2
mcgirtc7353-cmyk <mcgirtc7353@student.faytechcc.edu>
2026-02-18 11:45:54 -0500

    Add files via upload

---
+++ P2LAB2.py
@@ -0,0 +1,23 @@
+#Cedric McGirtc
+#2/18/26
+#P2LAB2
+# writing a program that creates a dictionary to store MPG values 
+
+#create dictionary 
+cars = {
+"camaro": 18.21,
+"pirus": 52.36,
+"Model S": 110,
+"silverado": 26}
+
+
+# Get and print dictionary keys 
+print()
+keys = cars.key()
+print(keys)
+print()
+#prompt user to choose a vehicle
+vehicle =input("Enter a vehicle to see its mpg")
+
+# Display MPG for chosen vehicle 
+mpg = cars{vehicle}
